844f0abac8ca691f4b558280420c4a5a5839982d
Geraldo Luiz <geraldobl58@gmail.com>
2026-09-22 13:16:38 -0300

    feat: add methods to retrieve associated OrderItems and Products in Order and Product entities

--- src/main/java/com/commerce/ecom/entities/Product.java
+++ src/main/java/com/commerce/ecom/entities/Product.java
@@ -3,6 +3,7 @@ package com.commerce.ecom.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -82,5 +83,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(OrderItem::getOrder).toList();
     }
 }
